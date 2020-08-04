c57a458f7a5e5d755b0c9caaddcf3ca5f84a481b
JakubMizera <kubamiz53@gmail.com>
2020-08-04 12:03:18 +0200

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../scss/main.scss";
+
+
+const Header = () => {
+    return (
+        <header>
+
+        </header>
+    )
+
+}
+
+
+const App = () => {
+    return (
+        <Header/>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector(".container"));
